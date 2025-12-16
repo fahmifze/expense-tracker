0897612cacac23950f9a308c41fe84e867ea26a6
ahmad fahmi <fahmi.fze@gmail.com>
2025-12-16 16:19:31 +0800

    feat: add environment variable for API URL

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
-const API_URL = '/api';
+const API_URL = import.meta.env.VITE_API_URL || '/api';
 
 // Create axios instance
 const api = axios.create({
